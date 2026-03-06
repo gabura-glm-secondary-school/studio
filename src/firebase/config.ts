9f62432df58c61fc82edc6f2db6e993e2e2270ce
Char Dayaler Pathsala <chardayalerpathsala@gmail.com>
2026-03-06 07:07:56 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,6 +2,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyD6hlsOz1l3L2mzRpmY2-BXdnKS0mEIWLc",
   authDomain: "gglmss-a0e7e.firebaseapp.com",
+  databaseURL: "https://gglmss-a0e7e-default-rtdb.firebaseio.com",
   projectId: "gglmss-a0e7e",
   storageBucket: "gglmss-a0e7e.firebasestorage.app",
   messagingSenderId: "6044778544",
